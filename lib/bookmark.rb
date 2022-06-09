425d1b518ffcae1d4a67e8927d0ee47c62d3f4c2
Amir Khan <ak2022dev@gmail.com>
2022-06-09 17:01:34 +0100

    Setting up test environment to use test tables depending on environment variable

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -3,6 +3,17 @@ require 'pg'
 
 class Bookmark
   def self.all
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_manager')
+    end
+
+    result = connection.exec("SELECT * FROM bookmarks")
+    result.map { |bookmark| bookmark['url'] }
+
+=begin
+    # Was second version
     url_array = []
     conn = PG.connect( dbname: 'bookmark_manager')
     conn.exec( "SELECT url FROM bookmarks" ) do |result|
@@ -12,12 +23,16 @@ class Bookmark
         end
       end
     end
+=end
+=begin
+    # Was first version
     url_array
     # [
     #   "http://makersacademy.com",
     #   "http://destroyallsoftware.com",
     #   "http://google.com"
     # ]
+=end
   end
 end
 
